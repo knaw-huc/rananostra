734834b249319583ca32c90ff92baefa1e051fab
LB <lhuygi@users.noreply.github.com>
2018-05-16 17:26:47 +0200

    Converter to BIO format

---
+++ src/main/java/nl/knaw/huygens/pergamon/Tags.java
@@ -0,0 +1,67 @@
+package nl.knaw.huygens.pergamon;
+
+import opennlp.tools.util.Span;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.util.Comparator.*;
+import static java.util.Comparator.comparing;
+
+/**
+ * Static utilities for working with tags.
+ */
+public class Tags {
+  /**
+   * Converts a list of entity spans to a list spans with BIO tags.
+   *
+   * @param entities Spans denoting entity locations, sorted by start index.
+   * @param tokens   Spans denoting token boundaries, sorted by start index.
+   * @return A list of spans, of length {@code tokens.size()}, with BIO tags indicating entity locations.
+   */
+  public static List<Span> toBIO(List<Span> entities, List<Span> tokens) {
+    List<Span> bio = new ArrayList<>(tokens.size());
+    for (int i = 0; i < tokens.size(); i++) {
+      bio.add(null);
+    }
+
+    for (Span e : entities) {
+      int pos = Collections.binarySearch(tokens, e, comparing(Span::getStart));
+      if (pos < 0) {
+        pos = -pos - 1;
+      }
+      if (pos >= tokens.size()) {
+        throw new IllegalStateException("entity span out of range");
+      }
+
+      boolean begin = true;
+      while (true) {
+        Span tok = tokens.get(pos);
+        if (tok.getStart() >= e.getEnd()) {
+          break;
+        }
+        if (bio.get(pos) != null) {
+          throw new IllegalStateException("overlapping entities");
+        }
+        String type = (begin ? "B-" : "I-") + e.getType();
+        bio.set(pos, new Span(tok.getStart(), tok.getEnd(), type));
+        begin = false;
+        if (++pos >= tokens.size()) {
+          break;
+        }
+      }
+    }
+
+    for (int i = 0; i < bio.size(); i++) {
+      if (bio.get(i) == null) {
+        Span tok = tokens.get(i);
+        bio.set(i, new Span(tok.getStart(), tok.getEnd(), "O"));
+      }
+    }
+
+    return bio;
+  }
+}
---
+++ src/test/java/nl/knaw/huygens/pergamon/TagsTest.java
@@ -0,0 +1,33 @@
+package nl.knaw.huygens.pergamon;
+
+import opennlp.tools.util.Span;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+import static org.junit.Assert.assertEquals;
+
+public class TagsTest {
+  @Test
+  public void basic() {
+    List<Span> entities = asList(new Span(5, 10, "per"), new Span(12, 14, "ORG"));
+    List<Span> tokens = asList(
+      new Span(0, 5), new Span(5, 8), new Span(8, 10),
+      new Span(10, 12), new Span(12, 14), new Span(14, 20));
+
+    List<Span> bio = Tags.toBIO(entities, tokens);
+    List<Span> expected = asList(
+      new Span(0, 5, "O"), new Span(5, 8, "B-per"), new Span(8, 10, "I-per"),
+      new Span(10, 12, "O"), new Span(12, 14, "B-ORG"), new Span(14, 20, "O"));
+    assertEquals(expected, bio);
+  }
+
+  @Test(expected = IllegalStateException.class)
+  public void outofrange() {
+    List<Span> entities = asList(new Span(0, 3, "foo"), new Span(1, 4, "foo"));
+    Tags.toBIO(entities, singletonList(new Span(0, 4)));
+  }
+}
