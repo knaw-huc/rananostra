86c26ddf9e05d1b3e52b5663d44c86db812883be
LB <lhuygi@users.noreply.github.com>
2019-02-27 11:21:03 +0100

    Improve error reporting

--- src/main/java/nl/knaw/huc/rananostra/rest/FrogResource.java
+++ src/main/java/nl/knaw/huc/rananostra/rest/FrogResource.java
@@ -2,22 +2,27 @@ package nl.knaw.huc.rananostra.rest;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import nl.knaw.huc.rananostra.FrogSocketClient;
+import nu.xom.ParsingException;
+import nu.xom.XPathException;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 
 import java.util.Map;
+
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
 
 @Path("/")
 @Consumes(MediaType.APPLICATION_JSON)
 public class FrogResource {
   private final FrogSocketClient frog;
 
-  public FrogResource(String host, int port) {
+  FrogResource(String host, int port) {
     frog = new FrogSocketClient(host, port);
   }
 
@@ -43,6 +48,12 @@ public class FrogResource {
   @Produces(MediaType.APPLICATION_XML)
   @POST
   public String applyXML(Args args) throws Exception {
-    return frog.applyXML(args.xml, args.xpath, args.namespaces, args.starttag, args.endtag);
+    try {
+      return frog.applyXML(args.xml, args.xpath, args.namespaces, args.starttag, args.endtag);
+    } catch (ParsingException e) {
+      throw new WebApplicationException("XML parsing error: " + e.getMessage(), BAD_REQUEST);
+    } catch (XPathException e) {
+      throw new WebApplicationException("XPath error: " + e.getMessage(), BAD_REQUEST);
+    }
   }
 }
---
+++ src/test/java/nl/knaw/huc/rananostra/rest/FrogResourceTest.java
@@ -0,0 +1,46 @@
+package nl.knaw.huc.rananostra.rest;
+
+import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.WebApplicationException;
+
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class FrogResourceTest {
+  @Test
+  void xmlInputValidation() {
+    FrogResource resource = new FrogResource("", 12345); // host and port not used
+
+    FrogResource.Args working = new FrogResource.Args();
+    working.xml = "<p/>";
+    working.xpath = "//p";
+    working.starttag = "start";
+    working.endtag = "end";
+
+    WebApplicationException e = assertThrows(WebApplicationException.class, () -> {
+      FrogResource.Args args = copy(working);
+      args.xml = "";
+      resource.applyXML(args);
+    });
+    assertEquals(BAD_REQUEST, e.getResponse().getStatus());
+
+    assertThrows(WebApplicationException.class, () -> {
+      FrogResource.Args args = copy(working);
+      args.xpath = "";
+      resource.applyXML(args);
+    });
+    assertEquals(BAD_REQUEST, e.getResponse().getStatus());
+  }
+
+  private FrogResource.Args copy(FrogResource.Args args) {
+    FrogResource.Args copy = new FrogResource.Args();
+    copy.endtag = args.endtag;
+    copy.namespaces = args.namespaces;
+    copy.starttag = args.starttag;
+    copy.xml = args.xml;
+    copy.xpath = args.xpath;
+    return copy;
+  }
+}
